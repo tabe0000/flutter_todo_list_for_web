ad63be1c7ea1a7d9349c55ff9e5f8468b329826c
tabe <tabetabe.unity@gmail.com>
2019-10-08 17:02:32 +0900

    Rough layout completed

--- lib/main.dart
+++ lib/main.dart
@@ -3,15 +3,12 @@ import 'package:flutter/material.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Todo list for web',
-      theme: ThemeData(
-
-      ),
-      home: TaskList(),
+      theme: ThemeData(),
+      home: RootPage(),
     );
   }
 }
@@ -24,9 +21,31 @@ class RootPage extends StatefulWidget {
 class _RootPageState extends State<RootPage> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
-    );
+    return Scaffold(
+        appBar: AppBar(
+          title: Text("Flutter Todo list"),
+          backgroundColor: Colors.black87,
+        ),
+        body: Row(
+          children: <Widget>[
+            Expanded(
+              flex: 5,
+              child: Container(
+                height: double.infinity,
+                width: double.infinity,
+                child: TaskList(),
+                color: Colors.white,
+              ),
+            ),
+            Expanded(
+              flex: 3,
+              child: Container(
+                child: InfoWindows(),
+                color: Colors.orange,
+              ),
+            )
+          ],
+        ));
   }
 }
 
@@ -36,10 +55,56 @@ class TaskList extends StatefulWidget {
 }
 
 class _TaskListState extends State<TaskList> {
+  List<String> tasks = [
+    "try making coffee",
+    "work on homework",
+    "do my paper",
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Container(
-      
+      color: Colors.green,
+      child: Column(
+        children: <Widget>[
+          Padding(
+            //Title Text
+            padding: EdgeInsets.only(top: 18.0, left: 13.0, bottom: 30.0),
+            child: Text("Today's tasks.",
+                style: TextStyle(
+                  fontSize: 30.0,
+                )
+            )
+          ),
+        ],
+      )
+    );
+  }
+}
+
+
+
+class InfoWindows extends StatefulWidget {
+  @override
+  _InfoWindowsState createState() => _InfoWindowsState();
+}
+
+class _InfoWindowsState extends State<InfoWindows> {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        Expanded(
+            flex: 1,
+            child: Container(
+              color: Colors.red,
+            )),
+        Expanded(
+            flex: 1,
+            child: Container(
+              color: Colors.blueGrey,
+            ))
+      ],
     );
   }
 }
